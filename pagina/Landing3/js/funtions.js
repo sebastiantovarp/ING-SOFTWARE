9fe1524666974379b5a371beebc9e49deff8277f
roxer8 <92970782+roxer8@users.noreply.github.com>
2023-04-18 18:44:58 -0500

    Add files via upload

---
+++ pagina/Landing3/js/funtions.js
@@ -0,0 +1,19 @@
+
+var boxcontainer = document.getElementById("box1");
+
+boxcontainer.addEventListener("click", function() {
+    boxcontainer.classList.toggle("box-clicked");
+});
+
+var boxcontainer = document.getElementById("box1");
+
+boxcontainer.addEventListener("click", function() {
+    boxcontainer.classList.toggle("box-clicked");
+});
+
+var boxcontainer = document.getElementById("box1");
+
+boxcontainer.addEventListener("click", function() {
+    boxcontainer.classList.toggle("box-clicked");
+});
+
