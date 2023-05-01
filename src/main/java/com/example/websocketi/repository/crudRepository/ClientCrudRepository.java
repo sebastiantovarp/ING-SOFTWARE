7413264247684bd830da98b9f9cb75cd6baf7660
juan varon <juandiegocarong@gmail.com>
2023-05-01 18:28:07 -0500

    Agregue el login sin las APIs de Google

---
+++ src/main/java/com/example/websocketi/repository/crudRepository/ClientCrudRepository.java
@@ -0,0 +1,19 @@
+package com.example.websocketi.repository.crudRepository;
+
+import com.example.websocketi.model.Client;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientCrudRepository extends CrudRepository<Client,Integer> {
+
+    @Query("SELECT c FROM Client AS c WHERE c.usuario =:user AND c.password =:password")
+    public Client getUser(String user, String password);
+
+    @Query("SELECT c FROM Client AS c WHERE c.usuario LIKE :user")
+    public  List<Client> getUserClient(String user);
+
+
+}
