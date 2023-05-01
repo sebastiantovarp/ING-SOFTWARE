e8459362eaab13347870c47907de4044c78230c0
juan varon <juandiegocarong@gmail.com>
2023-05-01 17:24:26 -0500

    Agregue el login sin las APIs de Google

--- IngSoftware/appCalendar/src/main/java/ingsof/calendar/appCalendar/repository/UserCrudRepositoy.java
+++
@@ -1,8 +0,0 @@
-package ingsof.calendar.appCalendar.repository;
-
-import ingsof.calendar.appCalendar.modeller.UserModeller;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserCrudRepositoy extends CrudRepository<UserModeller, Integer> {
-
-}
